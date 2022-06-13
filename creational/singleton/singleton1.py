33a5a4e9f909914c006bf015f6a623d98c7566b4
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-06-13 10:06:20 +0300

    add singleton1.py - реализация с помощью метода отложенной инициализации экземпляра

---
+++ creational/singleton/singleton1.py
@@ -0,0 +1,27 @@
+"""
+    Реализация паттерна Singleton по методу "отложенная инициализация экземпляра"
+
+    Например, в случае импорта модулей мы можем автоматически создать объект, даже если он не нужен.
+    Отложенное создание экземпляра гарантирует, что объект создается, только тогда,
+    когда он действительно необходим.
+"""
+
+class Singleton:
+
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            print('__init__ method called...')
+        else:
+            print('Instance already created: ', self.getInstance())
+
+    @classmethod
+    def getInstance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+s = Singleton()
+print('Object created ', Singleton.getInstance())
+s1 = Singleton()
