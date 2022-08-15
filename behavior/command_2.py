bf6f7ec66d95685500ea848c371d490c3a106c6c
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-08-15 15:58:28 +0300

    update command_2.py

--- behavior/command_2.py
+++ behavior/command_2.py
@@ -3,22 +3,32 @@
 	Этот вариант также соответствует паттерну Компановщик, т.к. и отдельные команды, и макрос команд имеют
 	одинаковый интерфейс (методы do() и undo())
 """
+import time
+
+
 class Grid:
 	def __init__(self, width: int, height: int) -> None:
-		self.__cells = [['white' for _ in range(height)] for _ in range(width)]
+		self._cells = [['white' for _ in range(height)] for _ in range(width)]
 
 	def cell(self, x: int, y: int, color: str = None):
 		if color is None:
-			return self.__cells[x][y]
-		self.__cells[x][y] = color
+			return self._cells[x][y]
+		self._cells[x][y] = color
+
+	def display(self):
+		for y in range(self.columns):
+			for x in range(self.rows):
+				print(self._cells[y][x].center(13), end='')
+			print('\n')
+		print('--------------------')
 
 	@property
 	def rows(self):
-		return len(self.__cells[0])
+		return len(self._cells[0])
 
 	@property
 	def columns(self):
-		return len(self.__cells)
+		return len(self._cells)
 
 
 class UndoableGrid(Grid):
@@ -72,16 +82,26 @@ if __name__ == '__main__':
 	grid = UndoableGrid(8, 3) 	# (1) пустая
 	redLeft = grid.create_cell_command(2, 1, 'red')
 	redRight = grid.create_cell_command(5, 0, 'red')
+	grid.display()
+	time.sleep(1)
 	redLeft() 					# (2) вставить красные ячейки
 	redRight.do() 				# ИЛИ: redRight()
 	greenLeft = grid.create_cell_command(2, 1, 'lightgreen')
 	greenLeft() 				# (3) вставить зеленую ячейку
+	grid.display()
+	time.sleep(1)
 	rectangleLeft = grid.create_rectangle_macro(1,1,2,2, 'lightblue')
 	rectangleRight = grid.create_rectangle_macro(5,0,6,1,'lightblue')
+	grid.display()
+	time.sleep(1)
 	rectangleLeft() 			# (4) Вставить синие ячейки
 	rectangleRight.do() 		# ИЛИ: rectangleRight()
+	grid.display()
+	time.sleep(1)
 	rectangleLeft.undo() 		# (5) отменить левую синию ячейку
 	greenLeft.undo() 			# (6) отменить левую зеленую ячейку
 	rectangleRight.undo() 		# (7) отменить правую синюю ячейку
 	redLeft.undo() 				# (8) отменить красные ячейки
 	redRight.undo()
+	grid.display()
+	time.sleep(1)
